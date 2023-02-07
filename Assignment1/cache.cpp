ef5ce8c82ab185147f7c4c1a34a99b8c28e82688
Pramodh-G <pramodh@iitk.ac.in>
2023-02-07 17:13:24 +0530

    add helper functions and code, better constructors

--- Assignment1/cache.cpp
+++ Assignment1/cache.cpp
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 
-#define L2_SETS 1024
-#define LOG_L2_SETS 10
-#define NUM_L2_TAGS 8
-#define L3_SETS 2048
-#define LOG_L3_SETS 11
-#define NUM_L3_TAGS 16
-#define LOG_BLOCK_SIZE 6
-#define L2_SET_BITS 0x3ff
-#define L3_SET_BITS 0x7ff
 #define ull unsigned long long
+constexpr int LOG_BLOCK_SIZE = 6;
+constexpr int L2_SETS = 1024;
+constexpr int NUM_L2_TAGS = 8;
+constexpr int LOG_L2_SETS = 10;
+constexpr ull L2_SET_BITS = 0x3ff;
+constexpr int L3_SETS = 2048;
+constexpr int NUM_L3_TAGS = 16;
+constexpr int LOG_L3_SETS = 11;
+constexpr ull L3_SET_BITS = 0x7ff;
 
 class Cache{
     public:
-        Cache(){
-            L2.resize(L2_SETS, std::vector<std::pair<ull, bool>> (NUM_L2_TAGS, {0, false}));
-            timeBlockAddedL2.resize(L2_SETS, std::vector <ull> (NUM_L2_TAGS, 0));
-            L3.resize(L3_SETS, std::vector<std::pair<ull, bool>> (NUM_L3_TAGS, {0, false}));
-            timeBlockAddedL3.resize(L3_SETS, std::vector <ull> (NUM_L3_TAGS, 0));
-        }
-        
+        Cache():
+            L2(L2_SETS, std::vector<std::pair<ull, bool>> (NUM_L2_TAGS, {0, false})),
+            timeBlockAddedL2(L2_SETS, std::vector <ull> (NUM_L2_TAGS, 0)),
+            L3(L3_SETS, std::vector<std::pair<ull, bool>> (NUM_L3_TAGS, {0, false})),
+            timeBlockAddedL3(L3_SETS, std::vector <ull> (NUM_L3_TAGS, 0)),
+            l2_hits(0), l2_misses(0), l3_hits(0), l3_misses(0) {}
+
         void simulator(char type, ull addr){
-            ull setL2 = (addr >> LOG_BLOCK_SIZE) & L2_SET_BITS;
-            ull tagL2 = (addr >> (LOG_BLOCK_SIZE + LOG_L2_SETS));
-            ull setL3 = (addr >> LOG_BLOCK_SIZE) & L3_SET_BITS;
-            ull tagL3 = (addr >> (LOG_BLOCK_SIZE + LOG_L3_SETS));
-            
+            auto [setL2, tagL2] = decode_address(addr, L2_SET_BITS, LOG_L2_SETS);
+            auto [setL3, tagL3] = decode_address(addr, L3_SET_BITS, LOG_L3_SETS);
+
             if(check_in_cache(setL2, tagL2, L2, NUM_L2_TAGS)){
                 l2_hits++;
                 update_priority(setL2, tagL2, L2, timeBlockAddedL2, NUM_L2_TAGS);
@@ -41,12 +39,10 @@ class Cache{
         }
 
     private:
-        
+
         unsigned long l2_hits, l2_misses, l3_hits, l3_misses;
-        std::vector <std::vector<std::pair<ull, bool>>> L2; // tag -> ull, active? -> bool
-        std::vector <std::vector<ull>> timeBlockAddedL2 ;
-        std::vector <std::vector<std::pair<ull, bool>>> L3; // tag -> ull, active? -> bool
-        std::vector <std::vector<ull>> timeBlockAddedL3 ;
+        std::vector <std::vector<std::pair<ull, bool>>> L2, L3; // tag -> ull, active? -> bool
+        std::vector <std::vector<ull>> timeBlockAddedL2, timeBlockAddedL3; // -> for eviction.
 
         bool check_in_cache(ull st, ull tag, std::vector <std::vector<std::pair<ull, bool>>> &Lx, int maxTags){
             for(int i = 0; i < maxTags; i++){
@@ -58,7 +54,7 @@ class Cache{
         }
 
         void copy_from_L3_and_replace_L2(ull setL2, ull tagL2, ull setL3, ull tagL3){
-            
+
         }
 
         void update_priority(ull st, ull tag, std::vector <std::vector<std::pair<ull, bool>>> &Lx, std::vector <std::vector<ull>> &timeBlockAddedLx, int maxTags){
@@ -68,6 +64,12 @@ class Cache{
                     timeBlockAddedLx[st][i] = maxValue + 1;
                 }
             }
+        }
+        std::pair<ull, ull>decode_address(ull addr, ull set_bits, int log_set_size)
+        {
+            ull st = (addr >> LOG_BLOCK_SIZE) & set_bits;
+            ull tag = (addr >> (LOG_BLOCK_SIZE + log_set_size));
+            return std::pair<ull, ull>(st, tag);
         }
 };
 

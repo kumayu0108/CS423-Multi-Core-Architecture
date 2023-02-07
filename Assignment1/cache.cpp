f92c1382ae89c61f79fa3975a35fab1e319f1cf7
Pramodh-G <pramodh@iitk.ac.in>
2023-02-07 20:18:28 +0530

    Add more helpers, setup inheritance, better design

--- Assignment1/cache.cpp
+++ Assignment1/cache.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 
 #define ull unsigned long long
+//defines a cache block. contains a tag and a valid bit.
+#define blk std::pair<ull, bool>
 constexpr int LOG_BLOCK_SIZE = 6;
 constexpr int L2_SETS = 1024;
 constexpr int NUM_L2_TAGS = 8;
@@ -14,72 +16,76 @@ constexpr ull L3_SET_BITS = 0x7ff;
 class Cache{
     public:
         Cache():
-            L2(L2_SETS, std::vector<std::pair<ull, bool>> (NUM_L2_TAGS, {0, false})),
+            L2(L2_SETS, std::vector<blk> (NUM_L2_TAGS, {0, false})),
             timeBlockAddedL2(L2_SETS, std::vector <ull> (NUM_L2_TAGS, 0)),
-            L3(L3_SETS, std::vector<std::pair<ull, bool>> (NUM_L3_TAGS, {0, false})),
+            L3(L3_SETS, std::vector<blk> (NUM_L3_TAGS, {0, false})),
             timeBlockAddedL3(L3_SETS, std::vector <ull> (NUM_L3_TAGS, 0)),
             l2_hits(0), l2_misses(0), l3_hits(0), l3_misses(0) {}
 
-        void simulator(char type, ull addr){
-            auto [setL2, tagL2] = decode_address(addr, L2_SET_BITS, LOG_L2_SETS);
-            auto [setL3, tagL3] = decode_address(addr, L3_SET_BITS, LOG_L3_SETS);
-
-            if(check_in_cache(setL2, tagL2, L2, NUM_L2_TAGS)){
-                l2_hits++;
-                update_priority(setL2, tagL2, L2, timeBlockAddedL2, NUM_L2_TAGS);
-            }
-            else {
-                l2_misses++;
-                if(check_in_cache(setL3, tagL3, L3, NUM_L3_TAGS)){
-                    l3_hits++;
-                    update_priority(setL3, tagL3, L3, timeBlockAddedL3, NUM_L3_TAGS);
-                    copy_from_L3_and_replace_L2(setL2, tagL2, setL3, tagL3);
-                }
-            }
-        }
+        virtual void simulator(char type, ull addr) = 0;
+        virtual void bring_from_memory(char type, ull addr) = 0;
+        virtual void bring_from_llc(char type, ull addr) = 0;
 
     private:
 
         unsigned long l2_hits, l2_misses, l3_hits, l3_misses;
-        std::vector <std::vector<std::pair<ull, bool>>> L2, L3; // tag -> ull, active? -> bool
+        std::vector <std::vector<blk>> L2, L3; // tag -> ull, active? -> bool
         std::vector <std::vector<ull>> timeBlockAddedL2, timeBlockAddedL3; // -> for eviction.
 
-        bool check_in_cache(ull st, ull tag, std::vector <std::vector<std::pair<ull, bool>>> &Lx, int maxTags){
+        // returns tag index in current set if found & is valid. Else returns -1
+        int check_in_cache(ull st, ull tag, std::vector <std::vector<blk>> &Lx, int maxTags){
             for(int i = 0; i < maxTags; i++){
                 if(Lx[st][i].second == true and Lx[st][i].first == tag){
-                    return true;
+                    return i;
                 }
             }
-            return false;
+            return -1;
         }
-
-        void copy_from_L3_and_replace_L2(ull setL2, ull tagL2, ull setL3, ull tagL3){
-
-        }
-
-        void update_priority(ull st, ull tag, std::vector <std::vector<std::pair<ull, bool>>> &Lx, std::vector <std::vector<ull>> &timeBlockAddedLx, int maxTags){
+        // updates LRU list of times.
+        void update_priority(ull st, ull tag, std::vector <std::vector<blk>> &Lx, std::vector <std::vector<ull>> &timeBlockAddedLx, int maxTags){
             auto maxValue = *(std::max_element(timeBlockAddedLx[st].begin(), timeBlockAddedLx[st].end()));
-            for(int i = 0; i < maxTags; i++){
-                if(Lx[st][i].second == true and Lx[st][i].first == tag){
-                    timeBlockAddedLx[st][i] = maxValue + 1;
-                }
-            }
+            int tag_index = check_in_cache(st, tag, Lx, maxTags);
+            if(tag_index != -1) timeBlockAddedLx[st][tag_index] = maxValue + 1;
         }
-        std::pair<ull, ull>decode_address(ull addr, ull set_bits, int log_set_size)
-        {
+        //helper function for decoding addresses
+        std::pair<ull, ull> decode_address(ull addr, ull set_bits, int log_set_size){
             ull st = (addr >> LOG_BLOCK_SIZE) & set_bits;
             ull tag = (addr >> (LOG_BLOCK_SIZE + log_set_size));
             return std::pair<ull, ull>(st, tag);
         }
+        // evicts a block by simply zeroing out its valid bit, given a tag and set.
+        void evict(ull st, ull tag, std::vector <std::vector<blk>> &Lx, int maxTags){
+            int tag_index = check_in_cache(st, tag, Lx, maxTags);
+            if(tag_index != -1) Lx[st][tag_index].second = false;
+        }
+        // given a certain set, it uses LRU policy to replace cache block.
+        void replace(ull st, ull tag, std::vector <std::vector<blk>> &Lx, std::vector <std::vector<ull>> &timeBlockAddedLx, int maxTags){
+            //find first invalid block, fill it, return.
+            int replaceIndex = -1;
+            for(int i = 0; i < maxTags; i++){
+                if(!Lx[st][i].second) // not valid;
+                {
+                    replaceIndex = i;
+                    break;
+                }
+            }
+            // else calculate index of minimum timestamp for LRU replacement.
+            if(replaceIndex == -1) replaceIndex = std::min_element(timeBlockAddedLx[st].begin(), timeBlockAddedLx[st].end()) - timeBlockAddedLx[st].begin();
+            Lx[st][replaceIndex] = blk(tag, true);
+            update_priority(st, tag, Lx, timeBlockAddedLx, maxTags);
+        }
 };
-
+//TODO: write simulate, bring from LLC, bring from memory for each of these caches.
+class NINECache : public Cache {};
+class IncCache : public Cache {};
+class ExCache : public Cache {};
 int main(int argc, char *argv[]){
     using std::cout;
     using std::endl;
     using std::vector;
     using std::pair;
 
-    Cache cache;
+    // Cache cache;
 
     FILE *fp;
     char input_name[256];
@@ -101,7 +107,7 @@ int main(int argc, char *argv[]){
             fread(&pc, sizeof(unsigned), 1, fp);
             std::cout << (int)i_or_d << " " << (int)type << " " << addr << " " << pc << std::endl;
             // Process the entry
-            cache.simulator(type, addr);
+            // cache.simulator(type, addr);
             return 0;
 
         }
@@ -110,6 +116,20 @@ int main(int argc, char *argv[]){
     }
     return 0;
 }
-/*
-2^19 / (2^6 * 8)
-*/
+        // void simulator(char type, ull addr){
+        //     auto [setL2, tagL2] = decode_address(addr, L2_SET_BITS, LOG_L2_SETS);
+        //     auto [setL3, tagL3] = decode_address(addr, L3_SET_BITS, LOG_L3_SETS);
+
+        //     if(check_in_cache(setL2, tagL2, L2, NUM_L2_TAGS)){
+        //         l2_hits++;
+        //         update_priority(setL2, tagL2, L2, timeBlockAddedL2, NUM_L2_TAGS);
+        //     }
+        //     else {
+        //         l2_misses++;
+        //         if(check_in_cache(setL3, tagL3, L3, NUM_L3_TAGS)){
+        //             l3_hits++;
+        //             update_priority(setL3, tagL3, L3, timeBlockAddedL3, NUM_L3_TAGS);
+        //             copy_from_L3_and_replace_L2(setL2, tagL2, setL3, tagL3);
+        //         }
+        //     }
+        // }

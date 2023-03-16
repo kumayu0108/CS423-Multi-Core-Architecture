4e5ecaf85ba0f490db1b8deb5a58c0e050828c37
Ayush Kumar <kumayu@iitk.ac.in>
2023-03-16 17:05:22 +0530

    minor changes

--- Assignment2/plot_cdf.py
+++ Assignment2/plot_cdf.py
@@ -1,6 +1,8 @@
 import re
 import matplotlib.pyplot as plt
 
+progName = "prog4"
+
 file1 = open('addrtrace.out', 'r')  
 totalAccGlobal, totalAccCache = 0, 0
 distanceMapGlobal, distanceMapCache = {}, {}
@@ -12,25 +14,25 @@ while True:
         matches = re.findall("[\d\.]+", line)
         totalAccGlobal += int(matches[1])
         distanceMapGlobal[float(matches[0])] = totalAccGlobal
-        print("{}".format(line.strip()), float(matches[0]), distanceMapGlobal[float(matches[0])])
+        # print("{}".format(line.strip()), float(matches[0]), distanceMapGlobal[float(matches[0])])
     elif line.startswith("Cache"):
         matches = re.findall("[\d\.]+", line)
         totalAccCache += int(matches[1])
         distanceMapCache[float(matches[0])] = totalAccCache
-        print("{}".format(line.strip()), float(matches[0]), distanceMapCache[float(matches[0])])
+        # print("{}".format(line.strip()), float(matches[0]), distanceMapCache[float(matches[0])])
 plt.figure(figsize=(10, 6))
 plt.plot(distanceMapGlobal.keys(), distanceMapGlobal.values())
 plt.title("cumulative density function for access distances (global)")
 plt.grid(True)
-plt.savefig("CDF_prog1_global")
+plt.savefig("CDF_{}_global".format(progName))
 
 plt.plot(distanceMapCache.keys(), distanceMapCache.values())
 plt.title("cumulative density function for access distances (both)")
 plt.grid(True)
-plt.savefig("CDF_prog1_both")
+plt.savefig("CDF_{}_both".format(progName))
 
 plt.clf()
 plt.plot(distanceMapCache.keys(), distanceMapCache.values())
 plt.title("cumulative density function for access distances (cache)")
 plt.grid(True)
-plt.savefig("CDF_prog1_cache")
+plt.savefig("CDF_{}_cache".format(progName))

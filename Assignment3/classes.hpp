97c938edd097ca37fcb781ddbd72e1d4fc60de7f
Pramodh-G <pramodh@iitk.ac.in>
2023-04-02 16:28:26 +0530

    move around DS

--- Assignment3/classes.hpp
+++ Assignment3/classes.hpp
@@ -36,24 +36,25 @@ class Inv : public Message {
 class Cache {
     protected:
         std::deque<Message> incomingMsg; // incoming messages from L2 and other L1s
+        std::vector<std::unordered_map<ull, ull>> CacheData;    // addr -> time map
+        std::vector <std::set<timeAddr>> timeBlockAdded;  // stores time and addr for eviction.
         virtual bool check_cache(ull addr) = 0;
+        virtual ull set_from_addr(ull addr) = 0;
         int id; // id of cache
 };
 class L1 : public Cache {
     int inputTrace; // from where you would read line to line
     Message *tempSpace; // when we deque, we need to store the top message.
-    std::vector<std::unordered_map<ull, ull>> L1CacheData;    // addr -> time map
-    std::vector<std::set<timeAddr>> timeBlockAddedL1;   // stores time and addr for eviction.
     inline ull set_from_addr(ull addr) { return ((addr << LOG_BLOCK_SIZE) & L1_SET_BITS); }
-    bool check_cache(ull addr){ return L1CacheData[set_from_addr(addr)].contains(addr); }
+    bool check_cache(ull addr){ return CacheData[set_from_addr(addr)].contains(addr); }
     public:
         L1(int id){
             this->id = id;
             std::string tmp = "traces/addrtrace_" + std::to_string(id) + ".out";
             this->inputTrace = open(tmp.c_str(), O_RDONLY);
             this->tempSpace = nullptr;
-            this->L1CacheData.resize(NUM_L1_SETS);
-            this->timeBlockAddedL1.resize(NUM_L1_SETS);
+            this->CacheData.resize(NUM_L1_SETS);
+            this->timeBlockAdded.resize(NUM_L1_SETS);
         }
         ~L1(){
             close(inputTrace);
@@ -61,15 +62,13 @@ class L1 : public Cache {
 };
 class LLC_bank : public Cache {
     // class storage structures for  directory??
-    std::vector<std::unordered_map<ull, ull>> L2CacheData;    // addr -> time map
-    std::vector <std::set<timeAddr>> timeBlockAddedL2;  // stores time and addr for eviction.
     inline ull set_from_addr(ull addr) { return ((addr << (LOG_BLOCK_SIZE + LOG_L2_BANKS)) & L2_SET_BITS); }
-    bool check_cache(ull addr){ return L2CacheData[set_from_addr(addr)].contains(addr); }
+    bool check_cache(ull addr){ return CacheData[set_from_addr(addr)].contains(addr); }
     public:
         LLC_bank(int id){
             this->id = id;
-            this->L2CacheData.resize(NUM_L2_SETS_PER_BANK);
-            this->timeBlockAddedL2.resize(NUM_L2_SETS_PER_BANK);
+            this->CacheData.resize(NUM_L2_SETS_PER_BANK);
+            this->timeBlockAdded.resize(NUM_L2_SETS_PER_BANK);
         }
         ~LLC_bank(){}
 };

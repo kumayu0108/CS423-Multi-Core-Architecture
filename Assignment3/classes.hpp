34788bbe12332318adc266821a9363f19972ecf6
Ayush Kumar <kumayu@iitk.ac.in>
2023-04-03 13:22:49 +0530

    more helper functions added

--- Assignment3/classes.hpp
+++ Assignment3/classes.hpp
@@ -5,11 +5,16 @@
 #include <vector>
 #include <unordered_map>
 #include <set>
+#include <iostream>
+#include <memory>
+#include <assert.h>
 
 using std::set, std::unordered_map, std::vector, std::deque;
+// using std::unique_ptr, std::make_unique;
 
 #define ull unsigned long long
 #define timeAddr std::pair<unsigned long, ull>
+#define blk std::pair<ull, bool>
 
 constexpr int NUM_L1_SETS = (1 << 6);   // each L1 cache
 constexpr ull L1_SET_BITS = 0x3f;
@@ -18,6 +23,7 @@ constexpr int L2_SET_BITS = 0x1ff;
 constexpr int LOG_L2_BANKS = 3;
 constexpr int MAX_PROC = 64;
 constexpr int LOG_BLOCK_SIZE = 6;
+constexpr int MAX_BUF_L1 = 10;
 int trace[MAX_PROC];
 
 // The data is logged in binary in the format of the following struct
@@ -27,59 +33,151 @@ struct LogStruct {
     ull addr;     // address (with last LOG_BLOCK_SIZE bits set to 0)
 };
 
+class Processor;    //forward declaration
+
 class Message {
-    int from, to; // store from and to cache id.
-    bool fromL1; // store if the message comes from l1 or l2. useful in l1 to l1 transfers
+    public:
+        enum MsgType { INV, NACK,  };
+        MsgType msgType;
+        int from, to; // store from and to cache id.
+        bool fromL1; // store if the message comes from l1 or l2. useful in l1 to l1 transfers
+        virtual void handle(Processor &proc){}
+        Message(){}
 };
 // just serves as an example to show inheritance model.
 class Inv : public Message {
     ull blockId; // which cache block to be evicted?
+    ull msgId;
+    public:
+        void handle(Processor &proc) {}
+        Inv(){}
 };
+class Nack : public Message {
+    public:
+        void handle(Processor &proc) {}
+        Nack(){}
+};
+class Get : public Message {
+    public:
+        void handle(Processor &proc) {}
+        Get(){}
+};
+class Getx : public Message {
+    public:
+        void handle(Processor &proc) {}
+        Getx(){}
+};
+class Put : public Message {
+    public:
+        void handle(Processor &proc) {}
+        Put(){}
+};
+class Putx : public Message {
+    public:
+        void handle(Processor &proc) {}
+        Putx(){}
+};
+class Upgr : public Message {
+    public:
+        void handle(Processor &proc) {}
+        Upgr(){}
+};
+
 class Cache {
     protected:
-        deque<Message> incomingMsg; // incoming messages from L2 and other L1s
-        vector<unordered_map<ull, ull>> CacheData;    // addr -> time map
-        vector <set<timeAddr>> timeBlockAdded;  // stores time and addr for eviction.
+        deque<Message *> incomingMsg; // incoming messages from L2 and other L1s
+        vector<unordered_map<ull, ull>> cacheData;    // addr -> time map
+        vector<set<timeAddr>> timeBlockAdded;  // stores time and addr for eviction.
+        int id; // id of cache
         virtual bool check_cache(ull addr) = 0;
         virtual ull set_from_addr(ull addr) = 0;
-        int id; // id of cache
-
+        // this function only removes this addr from cache & returns if anything got evicted
+        bool evict(ull st, ull addr){        
+            assert(st < cacheData.size());
+            if(!cacheData[st].contains(addr)){ return false; }
+            int time = cacheData[st][addr];
+            assert(timeBlockAdded[st].contains({time, addr}));
+            timeBlockAdded[st].erase({time, addr});
+            return true;
+        }
+        // this function replaces this addr from cache and returns the replaced address & flag stating valid block
+        blk replace(ull st, ull addr, ull timeAdded, int maxSetSize){
+            assert(st < cacheData.size());
+            if(cacheData[st].size() < maxSetSize){cacheData[st][addr] = timeAdded; return {0, false};}
+            assert(cacheData[st].size() == maxSetSize and timeBlockAdded[st].size() == maxSetSize);
+            auto [time_evicted, addr_evicted] = *timeBlockAdded[st].begin();
+            assert(time_evicted < timeAdded);
+            timeBlockAdded.erase(timeBlockAdded.begin());
+            cacheData[st].erase(addr_evicted);
+            cacheData[st][addr] = timeAdded;
+            timeBlockAdded[st].insert({timeAdded, addr});
+            return {addr_evicted, true};
+        }
     public:
-        Cache(int id, int NUM_SETS): id(id), CacheData(NUM_SETS), timeBlockAdded(NUM_SETS) {};
+        Cache(int id, int numSets): id(id), cacheData(numSets), timeBlockAdded(numSets) {
+            
+        };
 };
 class L1 : public Cache {
-    int inputTrace; // from where you would read line to line
-    Message *tempSpace; // when we deque, we need to store the top message.
-    inline ull set_from_addr(ull addr) { return ((addr << LOG_BLOCK_SIZE) & L1_SET_BITS); }
-    bool check_cache(ull addr){ return CacheData[set_from_addr(addr)].contains(addr); }
+    private:
+        int inputTrace; // from where you would read line to line
+        char buffer[MAX_BUF_L1 * sizeof(LogStruct) + 2];
+        deque<LogStruct> logs;
+        Message *tempSpace; // when we deque, we need to store the top message.
+        inline ull set_from_addr(ull addr) { return ((addr << LOG_BLOCK_SIZE) & L1_SET_BITS); }
+        bool check_cache(ull addr){ return cacheData[set_from_addr(addr)].contains(addr); }
+        void read_if_reqd(){
+            if(!logs.empty()){ return; }
+            int numBytesRead = read(inputTrace, buffer, MAX_BUF_L1 * sizeof(LogStruct));
+            int ind = 0;
+            while(ind < numBytesRead and ind + sizeof(LogStruct) <= numBytesRead){
+                LogStruct *tmpStruct = (LogStruct *)&buffer[ind];
+                logs.push_back(*tmpStruct);
+                ind += sizeof(LogStruct);
+            }
+        }
     public:
-        L1(int id): Cache(id, NUM_L1_SETS), tempSpace(nullptr) {
+        L1(int id): Cache(id, NUM_L1_SETS) {
             std::string tmp = "traces/addrtrace_" + std::to_string(id) + ".out";
             this->inputTrace = open(tmp.c_str(), O_RDONLY);
         }
         ~L1(){ close(inputTrace); }
 };
-class LLC_bank : public Cache {
+class LLCBank : public Cache {
     // class storage structures for  directory??
     inline ull set_from_addr(ull addr) { return ((addr << (LOG_BLOCK_SIZE + LOG_L2_BANKS)) & L2_SET_BITS); }
-    bool check_cache(ull addr){ return CacheData[set_from_addr(addr)].contains(addr); }
+    bool check_cache(ull addr){ return cacheData[set_from_addr(addr)].contains(addr); }
     public:
-        LLC_bank(int id): Cache(id, NUM_L2_SETS_PER_BANK) {}
-        ~LLC_bank(){}
+        LLCBank(int id): Cache(id, NUM_L2_SETS_PER_BANK) {}
+        ~LLCBank(){}
 };
 
 class Processor {
-    int  numCaches; // number of L1 and LLC cache;
+    int numCaches; // number of L1 and LLC cache;
+    int numCycles; // number of Cycles
     vector<L1> L1Caches;
-    vector<LLC_bank> L2Caches;
+    vector<LLCBank> L2Caches;
     public:
-        Processor() = delete; // delete default constructor
         Processor(int nCaches): numCaches(nCaches) {
             for(int i = 0; i < numCaches; i++) {
                 L1 ith_L1 = L1(i);
-                LLC_bank ith_L2 = LLC_bank(i);
-                L1Caches.push_back(ith_L1);
-                L2Caches.push_back(ith_L2);
+                LLCBank ith_L2 = LLCBank(i);
+                L1Caches.push_back(std::move(L1(i)));
+                L2Caches.push_back(std::move(LLCBank(i)));
+            }
+        }
+        void run(){
+            while(true){
+                bool progressMade = false;
+                for(int i = 0; i < numCaches; i++){
+                    // process L1    
+
+                }
+                for(int i = 0; i < numCaches; i++){
+                    // process L2
+                    
+                }
+                if(!progressMade){break;}
             }
         }
 };
--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -2,14 +2,15 @@
 #include <vector>
 
 // whatever driver code we need.
-
-Processor proc;
 int numCaches;
+
 int main(int argc, char *argv[]){
     if(argc != 2){
         printf("Enter Number of traces to process!");
         exit(1);
     }
+
     numCaches = atoi(argv[1]);
-    proc = Processor(numCaches);
+    Processor proc(numCaches);
+    // proc.run();
 }

b00e54d42cbe2d0ae471374707017af17a14cd0b
Ayush Kumar <kumayu@iitk.ac.in>
2023-04-09 23:42:50 +0530

    Implemented Nack struct and some TODOs

--- Assignment3/classes.hpp
+++ Assignment3/classes.hpp
@@ -70,17 +70,19 @@ class Processor;    //forward declaration
 
 class Message {
     public:
-        ull msgId; // Message id should be here, since before processing a message we need to check if we can process it?
+        // Message id should be here, since before processing a message we need to check if we can process it?
+        // commenting for now
+        // ull msgId; 
         MsgType msgType;
         int from, to; // store from and to cache id.
         bool fromL1; // store if the message comes from l1 or l2. useful in l1 to l1 transfers
         virtual void handle(Processor &proc, bool toL1){}
         Message(const Message&) = delete; // delete copy ctor explicitly since Message is a move only cls.
         Message& operator=(const Message&) = delete; // delete copy assignment ctor too.
-        Message() : msgType(MsgType::NACK), from(0), to(0), fromL1(false), msgId(0) {}
-        Message(MsgType msgType, int from, int to, bool fromL1, ull msgId) :
-            msgType(msgType), from(from), to(to), fromL1(fromL1), msgId(msgId) {}
-        Message(Message && other) noexcept : msgId(move(other.msgId)),
+        Message() : msgType(MsgType::NACK), from(0), to(0), fromL1(false) {}
+        Message(MsgType msgType, int from, int to, bool fromL1) :
+            msgType(msgType), from(from), to(to), fromL1(fromL1) {}
+        Message(Message && other) noexcept : 
             msgType(move(other.msgType)),
             from(move(other.from)),
             to(move(other.to)),
@@ -96,42 +98,56 @@ class Inv : public Message {
         Inv& operator=(const Inv&) = delete; // delete copy assignment ctor too.
         Inv() : Message(), blockAddr(0) {}
         Inv(Inv&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)) {}
-        Inv(MsgType msgType, int from, int to, bool fromL1, ull msgId, ull blockId) :
-            Message(msgType, from, to, fromL1, msgId), blockAddr(blockId) {}
+        Inv(MsgType msgType, int from, int to, bool fromL1, ull blockId) :
+            Message(msgType, from, to, fromL1), blockAddr(blockId) {}
 };
 class InvAck : public Message {
     public:
         ull blockAddr; // which cache block ADDR to be evicted?
         void handle(Processor &proc, bool toL1);
-        InvAck(const Inv&) = delete; // delete copy ctor explicitly, since it's a move only cls.
-        InvAck& operator=(const Inv&) = delete; // delete copy assignment ctor too.
+        InvAck(const InvAck&) = delete; // delete copy ctor explicitly, since it's a move only cls.
+        InvAck& operator=(const InvAck&) = delete; // delete copy assignment ctor too.
         InvAck() : Message() {}
-        InvAck(Inv&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)) {}
-        InvAck(MsgType msgType, int from, int to, bool fromL1, ull msgId, ull blockId) :
-            Message(msgType, from, to, fromL1, msgId), blockAddr(blockId) {}
+        InvAck(InvAck&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)) {}
+        InvAck(MsgType msgType, int from, int to, bool fromL1, ull blockId) :
+            Message(msgType, from, to, fromL1), blockAddr(blockId) {}
 };
 
 class Get : public Message {
     public:
         ull blockAddr; // which cache block ADDR to be evicted?
         void handle(Processor &proc, bool toL1);
-        Get(const Inv&) = delete; // delete copy ctor explicitly, since it's a move only cls.
-        Get& operator=(const Inv&) = delete; // delete copy assignment ctor too.
+        Get(const Get&) = delete; // delete copy ctor explicitly, since it's a move only cls.
+        Get& operator=(const Get&) = delete; // delete copy assignment ctor too.
         Get() : Message() {}
-        Get(Inv&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)) {}
-        Get(MsgType msgType, int from, int to, bool fromL1, ull msgId, ull blockId) :
-            Message(msgType, from, to, fromL1, msgId), blockAddr(blockId) {}
+        Get(Get&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)) {}
+        Get(MsgType msgType, int from, int to, bool fromL1, ull blockId) :
+            Message(msgType, from, to, fromL1), blockAddr(blockId) {}
 };
 
 class Put: public Message {
-    ull blockAddr; // which cache block ADDR to be evicted?
-    void handle(Processor &proc, bool toL1);
-    Put(const Inv&) = delete; // delete copy ctor explicitly, since it's a move only cls.
-    Put& operator=(const Inv&) = delete; // delete copy assignment ctor too.
-    Put() : Message() {}
-    Put(Inv&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)) {}
-    Put(MsgType msgType, int from, int to, bool fromL1, ull msgId, ull blockId) :
-    Message(msgType, from, to, fromL1, msgId), blockAddr(blockId) {}
+    public:
+        ull blockAddr; // which cache block ADDR to be evicted?
+        void handle(Processor &proc, bool toL1);
+        Put(const Put&) = delete; // delete copy ctor explicitly, since it's a move only cls.
+        Put& operator=(const Put&) = delete; // delete copy assignment ctor too.
+        Put() : Message() {}
+        Put(Put&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)) {}
+        Put(MsgType msgType, int from, int to, bool fromL1, ull blockId) :
+        Message(msgType, from, to, fromL1), blockAddr(blockId) {}
+};
+
+class Nack: public Message {
+    public:
+        ull blockAddr; // which cache block ADDR to be evicted?
+        MsgType nackedMsg;
+        void handle(Processor &proc, bool toL1);
+        Nack(const Nack&) = delete; // delete copy ctor explicitly, since it's a move only cls.
+        Nack& operator=(const Nack&) = delete; // delete copy assignment ctor too.
+        Nack() : Message() {}
+        Nack(Nack&& other) noexcept : Message(move(other)), blockAddr(move(other.blockAddr)), nackedMsg(move(other.nackedMsg)) {}
+        Nack(MsgType msgType, MsgType nackedMsg, int from, int to, bool fromL1, ull blockId) :
+        Message(msgType, from, to, fromL1), blockAddr(blockId), nackedMsg(nackedMsg) {}
 };
 
 class Cache {
@@ -142,7 +158,6 @@ class Cache {
         vector<set<timeAddr>> timeBlockAdded;  // stores time and addr for eviction.
         map<NACKStruct, int> outstandingNacks;
         unordered_map<ull, State> cacheState;
-        int nextGlobalMsgToProcess;
         int id; // id of cache
     public:
         unordered_map<ull, int> numInvToCollect;    // map from block address to number of invalidations to collect
@@ -176,13 +191,11 @@ class Cache {
         Cache(int id, int numSets): id(id),
             cacheData(numSets),
             timeBlockAdded(numSets),
-            nextGlobalMsgToProcess(0),
             numInvToCollect(0) {}
         Cache(Cache &&other) noexcept : incomingMsg(move(other.incomingMsg)),
             id(move(other.id)),
             cacheData(move(other.cacheData)),
             timeBlockAdded(move(other.timeBlockAdded)),
-            nextGlobalMsgToProcess(move(other.nextGlobalMsgToProcess)),
             numInvToCollect(move(other.numInvToCollect)),
             outstandingNacks(move(other.outstandingNacks)) {}
 };
@@ -246,12 +259,7 @@ class Processor {
         vector<L1> L1Caches;
         vector<LLCBank> L2Caches;
     public:
-        Processor(): numCycles(0) {
-            for(int i = 0; i < NUM_CACHE; i++) {
-                L1Caches.emplace_back(i);
-                L2Caches.emplace_back(LLCBank(i));
-            }
-        }
+        int nextGlobalMsgToProcess;
         void run(){
             while(true){
                 bool progressMade = false;
@@ -263,9 +271,21 @@ class Processor {
                     // process L2
                     progressMade |= L2Caches[i].process(*this);
                 }
+                numCycles++;
                 if(!progressMade){break;}
             }
         }
+        Processor(): numCycles(0), nextGlobalMsgToProcess(0) {
+            for(int i = 0; i < NUM_CACHE; i++) {
+                L1Caches.emplace_back(i);
+                L2Caches.emplace_back(LLCBank(i));
+            }
+        }
+        Processor(Processor&& other) noexcept: 
+            numCycles(move(other.numCycles)), 
+            L1Caches(move(other.L1Caches)),
+            L2Caches(move(other.L2Caches)),
+            nextGlobalMsgToProcess(move(other.nextGlobalMsgToProcess)) {}
 };
 
 // this wrapper would convert message to required type by taking ownership and then returns ownership
@@ -275,13 +295,16 @@ class Processor {
     msg.reset(static_cast<Message *>(inv_msg.release()))
 
 bool L1::process(Processor &proc){
-    if(incomingMsg.empty()){    // process message from trace
-        read_if_reqd();
-        if(logs.empty() or nextGlobalMsgToProcess < logs.front().time){ return false; }
-
+    bool progress = false;
+    read_if_reqd();
+    if(!logs.empty() and proc.nextGlobalMsgToProcess >= logs.front().time){ // process from trace
+        if(proc.nextGlobalMsgToProcess == logs.front().time){proc.nextGlobalMsgToProcess++;}
+        progress = true;
     }
-    else {
-        if(nextGlobalMsgToProcess < incomingMsg.front()->msgId){ return false; } // AYUSH: or should we then process messages from trace?
+    if(!incomingMsg.empty()){ 
+        progress = true;
+        // since msgId isn't associated with messages, we can always process a message.
+        // if(nextGlobalMsgToProcess < incomingMsg.front()->msgId){ return false; }
         auto msg = move(incomingMsg.front());
         incomingMsg.pop_front();
         // cast unique pointer of base class to derived class with appropriate variables
@@ -325,11 +348,12 @@ bool L1::process(Processor &proc){
             }
         }
     }
-    return false;
+    return progress;
 }
 
 bool LLCBank::process(Processor &proc){
-    if(nextGlobalMsgToProcess < incomingMsg.front()->msgId){ return false; } // AYUSH: or should we then process messages from trace?
+    // since msgId isn't associated with messages, we can always process a message.
+    // if(nextGlobalMsgToProcess < incomingMsg.front()->msgId){ return false; }
     auto msg = move(incomingMsg.front());
     incomingMsg.pop_front();
     // cast unique pointer of base class to derived class with appropriate variables
@@ -380,19 +404,21 @@ void Inv::handle(Processor &proc, bool toL1){
     // evict from L1
     l1.evict(l1.set_from_addr(blockAddr), blockAddr);
     if(fromL1){    // if the message is sent by L1, it means it is because someone requested S/I -> M
+        assert(toL1); // any invalidations sent by L1 would be sent to L1
         // generate the inv ack message to be sent to the 'from' L1 cache as directory informs cache about the receiver of ack in this way.
-        unique_ptr<Message> inv_ack(new InvAck(MsgType::INV_ACK, to, from, true, msgId, blockAddr));
+        unique_ptr<Message> inv_ack(new InvAck(MsgType::INV_ACK, to, from, true, blockAddr));
         proc.L1Caches[inv_ack->to].incomingMsg.push_back(move(inv_ack));
     }
     else {
         // generate inv ack to be sent to 'from' LLC
-        unique_ptr<Message> inv_ack(new InvAck(MsgType::INV_ACK, to, from, true, msgId, blockAddr));
+        unique_ptr<Message> inv_ack(new InvAck(MsgType::INV_ACK, to, from, true, blockAddr));
         proc.L2Caches[inv_ack->to].incomingMsg.push_back(move(inv_ack));
     }
 }
 
 void InvAck::handle(Processor &proc, bool toL1){
     if(fromL1){
+        assert(toL1);
         auto &l1 = proc.L1Caches[to];
         assert(l1.numInvToCollect.contains(blockAddr)); // since we collect an invalidation this means that this entry should contain this block address
         l1.numInvToCollect[blockAddr]--;
@@ -408,23 +434,31 @@ void InvAck::handle(Processor &proc, bool toL1){
 
 void Get::handle(Processor &proc, bool toL1){
     if(fromL1){ // this means message is sent to LLC bank
+        assert(!toL1); // L1 won't send Get to L1
         auto &l2 = proc.L2Caches[to];
         auto st = l2.set_from_addr(blockAddr);
         if(l2.directory[st].contains(blockAddr)){
             if(l2.directory[st][blockAddr].dirty){
                 int owner = l2.directory[st][blockAddr].ownerId;
-                unique_ptr<Message> get(new Get(MsgType::GET, to, owner, false, msgId, blockAddr));
-                proc.L1Caches[get->to].incomingMsg.push_back(move(get));
+                unique_ptr<Message> get(new Get(MsgType::GET, to, owner, false, blockAddr));
                 l2.directory[st][blockAddr].pending = true;
                 l2.directory[st][blockAddr].bitVector.reset();
                 l2.directory[st][blockAddr].bitVector.set(from);
                 l2.directory[st][blockAddr].bitVector.set(owner);
+                proc.L1Caches[get->to].incomingMsg.push_back(move(get));
             }
             else if(l2.directory[st][blockAddr].pending){
+                unique_ptr<Message> nack(new Nack(MsgType::NACK, MsgType::GET, to, from, false, blockAddr));
+                proc.L1Caches[nack->to].incomingMsg.push_back(move(nack));
+            }
+            else {
+                unique_ptr<Message> put(new Put(MsgType::PUT, to, from, false, blockAddr));
+                l2.directory[st][blockAddr].bitVector.set(from);
+                proc.L1Caches[put->to].incomingMsg.push_back(move(put));
             }
         }
         else if(l2.check_cache(blockAddr)) { // if present in cache but not in dir
-            unique_ptr<Message> put(new Put(MsgType::PUT, to, from, false, msgId, blockAddr));
+            unique_ptr<Message> put(new Put(MsgType::PUT, to, from, false, blockAddr));
             l2.directory[st][blockAddr].ownerId = from;
             l2.directory[st][blockAddr].dirty = true;
             l2.directory[st][blockAddr].pending = false;
@@ -433,6 +467,7 @@ void Get::handle(Processor &proc, bool toL1){
         }
         else {
             // TODO: evict, replace and update directory state.
+
         }
     }
     else {  // this means that message is an intervention
@@ -440,12 +475,12 @@ void Get::handle(Processor &proc, bool toL1){
     }
 }
 
-// Increase counter after processing.
+// Increase counter after processing. (done)
 // Maintain outstanding misses table.
-// remove message ID from message class, no need to look at order.In each cycle, exactly one machine access should be issued, increment counter.
-// For processing messages, in each cycle, deque one message from each queue, and process it.
-// Implement NACK. Remove lastMsgProcessed(Done, need to implement NACK).
+// remove message ID from message class, no need to look at order.In each cycle, exactly one machine access should be issued, increment counter. 
+// For processing messages, in each cycle, deque one message from each queue, and process it. (done)
+// Implement NACK. Remove lastMsgProcessed(Done, need to implement NACK). (done)
 // maintain a nastruct ck table, {GetX/Get, block} -> countdown_timer. map<NACKStruct, timer> (done)
 // Maintain separate cache state, per block inside L1.
 // Replace and Evict, make them virtual and implement them with messages for L1 and L2. Add appropriate asserts.
-// Add boolean variable to CALL_HANDLE macro (toL1, invalidations).
+// Add boolean variable to CALL_HANDLE macro (toL1, invalidations). (done)
